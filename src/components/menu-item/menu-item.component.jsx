3307272c2c3ea8967b1c7951c7752644a04a891e
Singh <amrsingh@lowes.com>
2020-02-13 05:33:26 -0500

    styles menu items with transition effects

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,6 +6,9 @@ const MenuItem = ({ title, imageUrl, size }) => (
         style={{backgroundImage: `url(${imageUrl})`}}
         className={`${size} menu-item`}
     >
+        <div className='background-image'
+             style={{backgroundImage: `url(${imageUrl})`}}
+        />
         <div className="content">
             <h1 className="title">{title}</h1>
             <span className="subtitle">SHOP NOW</span>
